bf2c88afaed39af7a5dc72c9f06cbffc00098c31
marwa <marwajawad19@gmail>
2021-08-24 18:33:45 -0400

    fix package.json file

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -3,5 +3,7 @@ import { Carousel } from "react-responsive-carousel";
 
 import { SearchItem } from "./SearchItem";
 export default function SearchList({ movies }) {
-  return movies.map((item, i) => <SearchItem key={i} item={item} />);
+  return movies
+    ? movies.map((item, i) => <SearchItem key={i} item={item} />)
+    : null;
 }
