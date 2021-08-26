609dfab8e6a870998e5d6a767d19ebd933634e60
marwa <marwajawad19@gmail.com>
2021-08-26 11:11:56 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { MoviesStore } from "./store";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import { Grid, GridItem } from "@chakra-ui/react";
-import SideBar from "./components/SideBar";
-import SearchList from "./components/SearchList";
+
 import { GetMovies } from "./store/Api";
 function App() {
   const { state, dispatch } = useContext(MoviesStore);
@@ -32,32 +29,7 @@ function App() {
     dispatch({ type: "Delete_Nomination", payload: updatedState });
   };
 
-  return (
-    <div className="App">
-      <Grid
-        h="200px"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(5, 1fr)"
-        gap={4}
-      >
-        <GridItem rowSpan={1} colSpan={1} bg="white">
-          <SideBar
-            items={state.nominations}
-            searchItem={searchItem}
-            setSearchItem={setSearchIte}
-            onDelete={onDelete}
-            onSearch={onSearch}
-          />
-        </GridItem>
-
-        <GridItem colSpan={4}>
-          <div className="MoviesList">
-            <SearchList movies={state.movies} onAdd={onAdd} />
-          </div>
-        </GridItem>
-      </Grid>
-    </div>
-  );
+  return <div className="App">let start it</div>;
 }
 
 export default App;
--- src/components/SearchList/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SearchItem } from "./SearchItem";
-export default function Index({ movies, onAdd }) {
-  return movies.map((item, i) => (
-    <SearchItem key={i} item={item} onAdd={() => onAdd(item)} />
-  ));
-}
